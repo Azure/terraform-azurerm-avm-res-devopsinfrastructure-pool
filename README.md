9ce73a63706b48ed6e41b2e2b09fb5e14d64e7f3
paul-e-martin <34447549+paul-e-martin@users.noreply.github.com>
2024-11-29 12:56:50 +0000

    feat: Added support for AzApi v 2.0 (#31)
    Co-authored-by: Paul Martin <paul.martin@daisyuk.tech>

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ The following requirements are needed by this module:
 
 - <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) (>= 1.9)
 
-- <a name="requirement_azapi"></a> [azapi](#requirement\_azapi) (~> 1.15)
+- <a name="requirement_azapi"></a> [azapi](#requirement\_azapi) (>= 1.13, < 3)
 
 - <a name="requirement_azurerm"></a> [azurerm](#requirement\_azurerm) (>= 3.116.0, < 5.0.0)
 
---
+++ examples/default-azapi-v2/main.tf
@@ -0,0 +1,203 @@
+variable "azure_devops_organization_name" {
+  type        = string
+  description = "Azure DevOps Organisation Name"
+}
+
+variable "azure_devops_personal_access_token" {
+  type        = string
+  description = "The personal access token used for authentication to Azure DevOps."
+  sensitive   = true
+}
+
+locals {
+  tags = {
+    scenario = "default"
+  }
+}
+
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2"
+    }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 1.1"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.5"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  azure_devops_organization_url = "https://dev.azure.com/${var.azure_devops_organization_name}"
+}
+
+provider "azuredevops" {
+  personal_access_token = var.azure_devops_personal_access_token
+  org_service_url       = local.azure_devops_organization_url
+}
+
+resource "random_string" "name" {
+  length  = 6
+  numeric = true
+  special = false
+  upper   = false
+}
+
+resource "azuredevops_project" "this" {
+  name = random_string.name.result
+}
+
+locals {
+  default_branch  = "refs/heads/main"
+  pipeline_file   = "pipeline.yml"
+  repository_name = "example-repo"
+}
+
+resource "azuredevops_git_repository" "this" {
+  project_id     = azuredevops_project.this.id
+  name           = local.repository_name
+  default_branch = local.default_branch
+  initialization {
+    init_type = "Clean"
+  }
+}
+
+resource "azuredevops_git_repository_file" "this" {
+  repository_id = azuredevops_git_repository.this.id
+  file          = local.pipeline_file
+  content = templatefile("${path.module}/${local.pipeline_file}", {
+    agent_pool_name = module.managed_devops_pool.name
+  })
+  branch              = local.default_branch
+  commit_message      = "[skip ci]"
+  overwrite_on_create = true
+}
+
+resource "azuredevops_build_definition" "this" {
+  project_id = azuredevops_project.this.id
+  name       = "Example Build Definition"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  repository {
+    repo_type   = "TfsGit"
+    repo_id     = azuredevops_git_repository.this.id
+    branch_name = azuredevops_git_repository.this.default_branch
+    yml_path    = local.pipeline_file
+  }
+}
+
+data "azuredevops_agent_queue" "this" {
+  project_id = azuredevops_project.this.id
+  name       = module.managed_devops_pool.name
+  depends_on = [module.managed_devops_pool]
+}
+
+resource "azuredevops_pipeline_authorization" "this" {
+  project_id  = azuredevops_project.this.id
+  resource_id = data.azuredevops_agent_queue.this.id
+  type        = "queue"
+  pipeline_id = azuredevops_build_definition.this.id
+}
+
+resource "azurerm_resource_group" "this" {
+  location = local.selected_region
+  name     = "rg-${random_string.name.result}"
+}
+
+locals {
+  resource_providers_to_register = {
+    dev_center = {
+      resource_provider = "Microsoft.DevCenter"
+    }
+    devops_infrastructure = {
+      resource_provider = "Microsoft.DevOpsInfrastructure"
+    }
+  }
+}
+
+data "azurerm_client_config" "this" {}
+
+resource "azapi_resource_action" "resource_provider_registration" {
+  for_each = local.resource_providers_to_register
+
+  resource_id = "/subscriptions/${data.azurerm_client_config.this.subscription_id}"
+  type        = "Microsoft.Resources/subscriptions@2021-04-01"
+  action      = "providers/${each.value.resource_provider}/register"
+  method      = "POST"
+}
+
+resource "azurerm_dev_center" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = "dc-${random_string.name.result}"
+  resource_group_name = azurerm_resource_group.this.name
+
+  depends_on = [azapi_resource_action.resource_provider_registration]
+}
+
+resource "azurerm_dev_center_project" "this" {
+  dev_center_id       = azurerm_dev_center.this.id
+  location            = azurerm_resource_group.this.location
+  name                = "dcp-${random_string.name.result}"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+# This is the module call
+module "managed_devops_pool" {
+  source                                   = "../.."
+  resource_group_name                      = azurerm_resource_group.this.name
+  location                                 = azurerm_resource_group.this.location
+  name                                     = "mdp-${random_string.name.result}"
+  dev_center_project_resource_id           = azurerm_dev_center_project.this.id
+  version_control_system_organization_name = var.azure_devops_organization_name
+  version_control_system_project_names     = [azuredevops_project.this.name]
+  enable_telemetry                         = var.enable_telemetry
+  tags                                     = local.tags
+  depends_on                               = [azapi_resource_action.resource_provider_registration]
+}
+
+output "managed_devops_pool_id" {
+  value = module.managed_devops_pool.resource_id
+}
+
+output "managed_devops_pool_name" {
+  value = module.managed_devops_pool.name
+}
+
+# Region helpers
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "0.3.0"
+}
+
+resource "random_integer" "region_index" {
+  max = length(local.regions) - 1
+  min = 0
+}
+
+locals {
+  excluded_regions = [
+    "westeurope" # Capacity issues
+  ]
+  included_regions = [
+    "australiaeast", "brazilsouth", "canadacentral", "centralus", "westeurope", "germanywestcentral", "italynorth", "japaneast", "uksouth", "eastus", "eastus2", "southafricanorth", "southcentralus", "southeastasia", "switzerlandnorth", "swedencentral", "westus3", "centralindia", "eastasia", "northeurope", "koreacentral"
+  ]
+  regions         = [for region in module.regions.regions : region.name if !contains(local.excluded_regions, region.name) && contains(local.included_regions, region.name)]
+  selected_region = "uksouth" # local.regions[random_integer.region_index.result]
+}

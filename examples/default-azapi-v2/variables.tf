78697b00f4ff3d4ab99c93d50b63dbf192d17952
Jared Holgate <jaredholgate@microsoft.com>
2025-06-18 09:52:58 +0100

    fix modtm issue

--- examples/default-azapi-v2/variables.tf
+++ examples/default-azapi-v2/variables.tf
@@ -1,3 +1,14 @@
+variable "azure_devops_organization_name" {
+  type        = string
+  description = "Azure DevOps Organisation Name"
+}
+
+variable "azure_devops_personal_access_token" {
+  type        = string
+  description = "The personal access token used for authentication to Azure DevOps."
+  sensitive   = true
+}
+
 variable "enable_telemetry" {
   type        = bool
   default     = true
@@ -6,22 +17,4 @@ This variable controls whether or not telemetry is enabled for the module.
 For more information see <https://aka.ms/avm/telemetryinfo>.
 If it is set to false, then no telemetry will be collected.
 DESCRIPTION
-}
-variable "azure_devops_organization_name" {
-  type        = string
-  description = "Azure DevOps Organisation Name"
-}
-variable "azure_devops_personal_access_token" {
-  type        = string
-  description = "The personal access token used for authentication to Azure DevOps."
-  sensitive   = true
-}
-variable "azure_devops_organization_name" {
-  type        = string
-  description = "Azure DevOps Organisation Name"
-}
-variable "azure_devops_personal_access_token" {
-  type        = string
-  description = "The personal access token used for authentication to Azure DevOps."
-  sensitive   = true
 }
